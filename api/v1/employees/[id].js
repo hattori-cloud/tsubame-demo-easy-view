9f1906889180cd9959e97d5b47c625df2e118088
hattori-cloud <hattori@tsubame.co.jp>
2026-09-23 15:09:21 +0900

    v183: centralize employee ETag handling

--- api/v1/employees/[id].js
+++ api/v1/employees/[id].js
@@ -1,5 +1,6 @@
 const {authenticateRequest,sendApiError}=require('../../_lib/auth');
 const {applySecurityHeaders,requestId}=require('../../_lib/security');
+const {setVersionEtag}=require('../../_lib/concurrency');
 const {resolveCurrentUser,getStagingEmployeeForUser}=require('../../_lib/authorization');
 
 module.exports=async function handler(req,res){
@@ -8,7 +9,7 @@ module.exports=async function handler(req,res){
     const identity=await authenticateRequest(req);
     const user=resolveCurrentUser(identity);
     const employee=getStagingEmployeeForUser(user,String(req.query.id||''));
-    const id=requestId(req);applySecurityHeaders(res);res.setHeader('X-Request-Id',id);res.setHeader('ETag','"'+employee.version+'"');
+    const id=requestId(req);applySecurityHeaders(res);res.setHeader('X-Request-Id',id);setVersionEtag(res,employee.version);
     return res.status(200).json({employee,data_mode:'fictional-staging-fixtures'})
   }catch(err){return sendApiError(req,res,err)}
 };
