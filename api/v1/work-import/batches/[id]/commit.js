ca07f1f8991c608ab27a4af1178eb5f790b33f37
hattori-cloud <hattori@tsubame.co.jp>
2026-09-25 17:51:46 +0900

    fix: correct work-import commit module paths

--- api/v1/work-import/batches/[id]/commit.js
+++ api/v1/work-import/batches/[id]/commit.js
@@ -1,8 +1,8 @@
-const {authenticateRequest,sendApiError}=require('../../../../../_lib/auth');
-const {resolveCurrentUser}=require('../../../../../_lib/authorization');
-const {applySecurityHeaders,requestId}=require('../../../../../_lib/security');
-const {parseIfMatchHeader,setVersionEtag}=require('../../../../../_lib/concurrency');
-const {commitWorkImport}=require('../../../../../_lib/work-import-store');
+const {authenticateRequest,sendApiError}=require('../../../../_lib/auth');
+const {resolveCurrentUser}=require('../../../../_lib/authorization');
+const {applySecurityHeaders,requestId}=require('../../../../_lib/security');
+const {parseIfMatchHeader,setVersionEtag}=require('../../../../_lib/concurrency');
+const {commitWorkImport}=require('../../../../_lib/work-import-store');
 
 module.exports=async function handler(req,res){
   if(req.method!=='POST'){res.setHeader('Allow','POST');return sendApiError(req,res,{status:405,code:'METHOD_NOT_ALLOWED',message:'POSTのみ利用できます'})}
