aa8ad4c98a81caef51d3da1392048aac7ff26dbe
hattori-cloud <hattori@tsubame.co.jp>
2026-09-26 17:20:19 +0900

    vehicles: include assignment history in detail response

--- api/v1/vehicles/[id].js
+++ api/v1/vehicles/[id].js
@@ -2,13 +2,14 @@ const {authenticateRequest,sendApiError}=require('../../_lib/auth');
 const {resolveCurrentUser}=require('../../_lib/authorization');
 const {applySecurityHeaders,requestId}=require('../../_lib/security');
 const {parseIfMatchHeader,setVersionEtag}=require('../../_lib/concurrency');
-const {getVehicle,updateVehicle}=require('../../_lib/vehicle-store');
+const {getVehicle,getVehicleAssignmentHistory,updateVehicle}=require('../../_lib/vehicle-store');
 
 module.exports=async function handler(req,res){
   try{
     const identity=await authenticateRequest(req),user=resolveCurrentUser(identity),id=String(req.query.id||''),rid=requestId(req);
     if(req.method==='GET'){
-      const vehicle=await getVehicle(user,id);applySecurityHeaders(res);res.setHeader('X-Request-Id',rid);setVersionEtag(res,vehicle.version);return res.status(200).json({vehicle})
+      const vehicle=await getVehicle(user,id),assignment_history=await getVehicleAssignmentHistory(user,id);
+      applySecurityHeaders(res);res.setHeader('X-Request-Id',rid);setVersionEtag(res,vehicle.version);return res.status(200).json({vehicle,assignment_history})
     }
     if(req.method==='PATCH'){
       const vehicle=await updateVehicle({user,id,body:req.body||{},expectedVersion:parseIfMatchHeader(req.headers['if-match']),requestId:rid});
